a9237efbf0c32942cf9160ce22ad47b8e3f2e37a
yapingxin <xinyp@live.com>
2017-01-13 02:17:52 +0800

    Change C++ ==> C project

---
+++ Daemon-Prototype/Server/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello from Server!\n");
+    return 0;
+}
--- Daemon-Prototype/Server/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-
-int main()
-{
-    printf("hello from Server!\n");
-    return 0;
-}
