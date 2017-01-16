b17460dbd96346fe3f9ba352feb54e181ca4d42c
yapingxin <xinyp@live.com>
2017-01-17 03:13:25 +0800

    /Code/utility/main.c

--- Daemon-Prototype/Server/Code/utility/main.c
+++ Daemon-Prototype/Server/Code/utility/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "logfunc.h"
-
 /* Socket Server Header Begin */
 #include <stdio.h>
 #include <sys/socket.h>
@@ -18,24 +16,67 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
+#include <inttypes.h>
+#include <linux/tcp.h> /* #define TCP_NODELAY 1 */
+
+#define MAXPENDING 5
+#define BUFSIZE 1024
+
+#include "logfunc.h"
+
 #define ISspace(x) isspace((int)(x))
 
 #define SERVER_STRING "Tint HTTP Application Server: 0.1.0\r\n"
 /* Socket Server Header End */
 
-static int startup(unsigned short *port);
 static void error_die(const char *sc);
-static void accept_request(int);
-static int get_line(int, char *, int);
+
+/*
+
+- socket
+  sockfd = socket(int socket_family, int socket_type, int protocol);
+  http://man7.org/linux/man-pages/man7/socket.7.html
+
+- setsockopt
+  int setsockopt(int sockfd, int level, int optname, const void *optval, socklen_t optlen);
+  http://man7.org/linux/man-pages/man2/setsockopt.2.html
+
+- bind
+  int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+  http://man7.org/linux/man-pages/man2/bind.2.html
+
+- listen - listen for connections on a socket
+  int listen(int sockfd, int backlog);
+  http://man7.org/linux/man-pages/man2/listen.2.html
+
+- accept, accept4 - accept a connection on a socket
+  int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+  http://man7.org/linux/man-pages/man2/accept.2.html
+  https://linux.die.net/man/2/accept
+
+- inet_ntop - convert IPv4 and IPv6 addresses from binary to text form
+  const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
+  http://man7.org/linux/man-pages/man3/inet_ntop.3.html
+  https://linux.die.net/man/3/inet_ntop
+
+*/
+
+static void handle_tcp_client(int client_sockfd);
+
 
 int main(int argc, char *argv[])
 {
-	int server_sock = -1;
-	unsigned short port = 8001;
-	int client_sock = -1;
-	struct sockaddr_in client_socket_addr = { 0 };
-	socklen_t client_socket_addr_len = sizeof(struct sockaddr_in);
-	pthread_t accept_request_thread = NULL;
+	uint16_t server_port = 8001;
+	int server_sockfd = 0;
+	int client_sockfd = 0;
+	struct sockaddr_in server_addr = { 0 };
+	struct sockaddr_in client_addr = { 0 };
+	socklen_t client_addr_len = sizeof(struct sockaddr_in);
+	int flag;
+	int ret;
+
+	char client_name[INET_ADDRSTRLEN] = { 0 };
+	const char *ret_constxt = NULL;
 
 	file_output_open(global_log_file_path);
 
@@ -46,106 +87,91 @@ int main(int argc, char *argv[])
 	ZF_LOGI("*****************************************************");
 	ZF_LOGI("");
 
-	ZF_LOGI("You will see the number of arguments: %i", argc);
-	ZF_LOGD("You will NOT see the first argument: %s", *argv);
+	//ZF_LOGI("You will see the number of arguments: %i", argc);
+	//ZF_LOGD("You will NOT see the first argument: %s", *argv);
 
-	ZF_LOGW("You will see this WARNING message");
-	ZF_LOGI("You will NOT see this INFO message");
+	//ZF_LOGW("You will see this WARNING message");
+	//ZF_LOGI("You will NOT see this INFO message");
 
-	server_sock = startup(&port);
+	server_sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (server_sockfd < 0)
+	{
+		error_die("socket() failed.");
+	}
+	else
+	{
+		ZF_LOGI("socket() ==> server_sockfd = 0x%08x", server_sockfd);
+	}
 
-	printf("http server running on port %d\n", port);
-	ZF_LOGI("http server running on port %i", port);
+	/* Disable the Nagle (TCP No Delay) algorithm */
+	flag = 1;
+	ret = setsockopt(server_sockfd, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(flag));
+	if (ret == -1)
+	{
+		ZF_LOGW("Couldn't setsockopt(TCP_NODELAY)");
+	}
+	else
+	{
+		ZF_LOGI("setsockopt(TCP_NODELAY) success.");
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(server_port);
+
+	ret = bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
+	if (ret < 0)
+	{
+		error_die("bind() failed.");
+	}
+	else
+	{
+		ZF_LOGI("bind() result: bind to port %i", server_port);
+		printf("bind() result: bind to port %d\n", server_port);
+	}
+
+	ret = listen(server_sockfd, MAXPENDING);
+	if (ret < 0)
+	{
+		error_die("listen() failed.");
+	}
+	else
+	{
+		ZF_LOGI("listen() at port %i with backlog = %i", server_port, MAXPENDING);
+		printf("listen() at port %d with backlog = %d\n", server_port, MAXPENDING);
+	}
+
+	ZF_LOGI("Server is ruinning.");
+	printf("Server is ruinning.\n");
 
 	while (1)
 	{
-		client_sock = accept(server_sock, (struct sockaddr *)&client_socket_addr, &client_socket_addr_len);
-		if (client_sock == -1)
+		client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+		if (client_sockfd < 0)
 		{
 			error_die("accept() failed.");
 		}
 
-		/* accept_request(&client_sock); */
-		if (pthread_create(&accept_request_thread, NULL, (void *)accept_request, (void *)&client_sock) != 0)
+		ret_constxt = inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_name, INET_ADDRSTRLEN);
+		if (ret_constxt != NULL)
 		{
-			error_die("pthread_create() failed.");
+			ZF_LOGI("Handling client %s/%i", client_name, ntohs(client_addr.sin_port));
+			printf("Handling client %s/%d\n", client_name, ntohs(client_addr.sin_port));
 		}
 
-		close(server_sock);
+		handle_tcp_client(client_sockfd);
 	}
 
-	printf("Server is ruinning.\n");
     return 0;
 }
 
-
-/**********************************************************************/
-/* This function starts the process of listening for web connections
-* on a specified port.  If the port is 0, then dynamically allocate a
-* port and modify the original port variable to reflect the actual
-* port.
-* Parameters: pointer to variable containing the port to connect on
-* Returns: the socket
-*
-* Links:
-* - setsockopt()--Set Socket Options
-*   https://www.ibm.com/support/knowledgecenter/en/ssw_i5_54/apis/ssocko.htm
-*   http://www.cnblogs.com/hateislove214/archive/2010/11/05/1869886.html
-*/
-/**********************************************************************/
-int startup(unsigned short *port)
-{
-	const int listen_backlog = 5;
-
-	int httpd = 0;
-	int option_value = 1;
-	struct sockaddr_in addr;
-
-	httpd = socket(PF_INET, SOCK_STREAM, 0);
-	if (httpd == -1)
-	{
-		error_die("socket");
-	}
-
-	memset(&addr, 0, sizeof(struct sockaddr_in));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(*port);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if ((setsockopt(httpd, SOL_SOCKET, SO_REUSEADDR, &option_value, sizeof(int))) < 0)
-	{
-		error_die("setsockopt failed");
-	}
-
-	if (bind(httpd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) < 0)
-	{
-		error_die("bind");
-	}
-
-	if (*port == 0)  /* if dynamically allocating a port */
-	{
-		socklen_t namelen = sizeof(struct sockaddr_in);
-		if (getsockname(httpd, (struct sockaddr *)&addr, &namelen) == -1)
-		{
-			error_die("getsockname");
-		}
-
-		*port = ntohs(addr.sin_port);
-	}
-
-	if (listen(httpd, listen_backlog) < 0)
-	{
-		error_die("listen");
-	}
-
-	return(httpd);
-}
 
 /**********************************************************************/
 /* Print out an error message with perror() (for system errors; based
 * on value of errno, which indicates system call errors) and exit the
 * program indicating an error. */
 /**********************************************************************/
-void error_die(const char *sc)
+static void error_die(const char *sc)
 {
 	ZF_LOGF("%s", sc);
 
@@ -155,83 +181,32 @@ void error_die(const char *sc)
 	exit(1);
 }
 
-/**********************************************************************/
-/* A request has caused a call to accept() on the server port to
-* return.  Process the request appropriately.
-* Parameters: the socket connected to the client */
-/**********************************************************************/
-static void accept_request(int client)
+
+static void handle_tcp_client(int client_sockfd)
 {
-	char accept_buf[1024] = { 0 };
-	size_t numchars;
-	char method[255] = { 0 };
-	char url[255] = { 0 };
-	char path[512] = { 0 };
-	size_t i, j;
-	struct stat st;
-	int cgi = 0;      /* becomes true if server decides this is a CGI
-					  * program */
-	char *query_string = NULL;
+	static char buffer[BUFSIZE] = { 0 };
 
-	ZF_LOGI("Enter accept_request with client = %i", client);
-	printf("Enter accept_request with client = %d\n", client);
+	size_t num_bytes_rcvd = 0;
 
-	while (numchars = get_line(client, accept_buf, 1024 * sizeof(char)) > 0)
+	num_bytes_rcvd = recv(client_sockfd, buffer, BUFSIZE, 0);
+	if (num_bytes_rcvd < 0)
 	{
-		ZF_LOGI("get_line: %s", accept_buf);
-		printf("get_line: %s\n", accept_buf);
+		error_die("recv() failed.");
 	}
 
-	ZF_LOGI("Completed accept_request with client = %i", client);
-	printf("Completed accept_request with client = %d\n", client);
-
-	close(client);
-}
-
-
-/**********************************************************************/
-/* Get a line from a socket, whether the line ends in a newline,
-* carriage return, or a CRLF combination.  Terminates the string read
-* with a null character.  If no newline indicator is found before the
-* end of the buffer, the string is terminated with a null.  If any of
-* the above three line terminators is read, the last character of the
-* string will be a linefeed and the string will be terminated with a
-* null character.
-* Parameters: the socket descriptor
-*             the buffer to save the data in
-*             the size of the buffer
-* Returns: the number of bytes stored (excluding null) */
-/**********************************************************************/
-int get_line(int sock, char *buf, int size)
-{
-	int i = 0;
-	char c = '\0';
-	int n;
-
-	printf("Enter get_line: size = %d\n", size);
-
-	while ((i < size - 1) && (c != '\n'))
+	/* if num_bytes_rcvd == 0 then the end of stream is reached. */
+	while (num_bytes_rcvd > 0)
 	{
-		n = recv(sock, &c, 1, 0);
-		/* DEBUG printf("%02X\n", c); */
-		if (n > 0)
+		ZF_LOGI("Recv: %s", buffer);
+		printf("Recv: %s\n", buffer);
+
+		num_bytes_rcvd = recv(client_sockfd, buffer, BUFSIZE, 0);
+		if (num_bytes_rcvd < 0)
 		{
-			if (c == '\r')
-			{
-				n = recv(sock, &c, 1, MSG_PEEK);
-				/* DEBUG printf("%02X\n", c); */
-				if ((n > 0) && (c == '\n'))
-					recv(sock, &c, 1, 0);
-				else
-					c = '\n';
-			}
-			buf[i] = c;
-			i++;
+			error_die("recv() failed.");
 		}
-		else
-			c = '\n';
 	}
-	buf[i] = '\0';
 
-	return(i);
+	close(client_sockfd);
 }
+
