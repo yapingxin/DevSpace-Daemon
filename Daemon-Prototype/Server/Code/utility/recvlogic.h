f2a866f3e071a0451ea00b4744b3e7851d5fda59
yapingxin <xinyp@live.com>
2017-01-17 11:40:52 +0800

    recvlogic.cpp

--- Daemon-Prototype/Server/Code/utility/recvlogic.h
+++ Daemon-Prototype/Server/Code/utility/recvlogic.h
@@ -7,4 +7,8 @@ void accept_request(int client_sockfd);
 
 int disable_tcp_nagle(int sockfd);
 
+void sendString(char *message, int socket);
+void sendHeader(char *Status_code, char *Content_Type, int TotalSize, int socket);
+void sendHTML(char *statusCode, char *contentType, char *content, int size, int socket);
+
 #endif // _UTILITY_RECVLOGIC_H_
