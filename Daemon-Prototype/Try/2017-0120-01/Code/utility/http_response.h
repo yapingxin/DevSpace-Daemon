f0f9b2ee7c54129ba1bdb3bafb0965388518dbe6
yapingxin <xinyp@live.com>
2017-01-22 18:14:34 +0800

    2017-0120-01

---
+++ Daemon-Prototype/Try/2017-0120-01/Code/utility/http_response.h
@@ -0,0 +1,16 @@
+#ifndef _UTILITY_HTTP_RESPONSE_H_
+#define _UTILITY_HTTP_RESPONSE_H_
+
+#include <stdio.h>
+
+#define SERVER_STRING "Server: Tiny HTTP Application Server/0.2.1\r\n"
+
+
+void unimplemented(int);
+void serve_file(int, const char *);
+void cat(int, FILE *);
+void execute_cgi(const int, const char *, const char *, const char *);
+
+void default_http_response(const int client_sockfd, const char * msg);
+
+#endif // _UTILITY_HTTP_RESPONSE_H_
